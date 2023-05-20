e1bbb67545d2867eec07f3598d994218d07abef5
Dmitry Kutakov <git@vkd.pm>
2023-05-20 12:48:26 +0200

    add test: request canceled

--- tests/response_default/test_test.go
+++ tests/response_default/test_test.go
@@ -1,6 +1,9 @@
 package test
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -17,4 +20,66 @@ func TestResponseDefault(t *testing.T) {
 
 	assert.Equal(t, 400, w.Code)
 	assert.Equal(t, "{\"message\":\"test default response\"}\n", w.Body.String())
+}
+
+type ResponseStatusWriter struct {
+	rCtx context.Context
+	http.ResponseWriter
+	status    int
+	statusOut chan<- int
+}
+
+func (r *ResponseStatusWriter) WriteHeader(s int) {
+	select {
+	case <-r.rCtx.Done():
+		s = 499
+	default:
+	}
+	r.status = s
+	r.statusOut <- s
+	r.ResponseWriter.WriteHeader(s)
+}
+
+func TestResponse_Canceled(t *testing.T) {
+	clientStart := make(chan struct{})
+
+	api := &API{
+		GetPetsHandler: func(r GetPetsRequester) GetPetsResponder {
+			req := r.Parse()
+			ctx := req.HTTPRequest.Context()
+
+			close(clientStart)
+			<-ctx.Done()
+			return GetPetsResponseDefaultJSON(500, Error{Message: "test default response"})
+		},
+	}
+	status := make(chan int, 1)
+	api.Middlewares = append(api.Middlewares, func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			h.ServeHTTP(&ResponseStatusWriter{
+				statusOut:      status,
+				ResponseWriter: rw,
+				rCtx:           r.Context(),
+			}, r)
+		})
+	})
+
+	srv := httptest.NewServer(api)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-clientStart
+		cancel()
+	}()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", srv.URL+"/pets", nil)
+	if err != nil {
+		t.Errorf("Error on create request: %v", err)
+	}
+
+	client := srv.Client()
+	_, err = client.Do(req)
+	assert.True(t, errors.Is(err, context.Canceled))
+
+	assert.Equal(t, 499, <-status)
 }
