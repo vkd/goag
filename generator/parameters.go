7129bbae417356f54e7f383d5b56954d4eafd439
Dmitry Kutakov <git@vkd.pm>
2025-02-16 14:15:41 +0100

    refactoring - path parameter

--- generator/parameters.go
+++ generator/parameters.go
@@ -38,7 +38,7 @@ type PathParameters []*PathParameter
 
 func (s PathParameters) Get(name string) (zero *PathParameter, _ error) {
 	for _, p := range s {
-		if p.s.Name == name {
+		if p.Name == name {
 			return p, nil
 		}
 	}
@@ -46,32 +46,32 @@ func (s PathParameters) Get(name string) (zero *PathParameter, _ error) {
 }
 
 type PathParameter struct {
-	s *specification.PathParameter
-
-	Name          string
-	FieldName     string
-	FieldTypeName string
-	Type          Schema
-	Description   string
+	Name        string
+	FieldName   string
+	Type        Schema
+	Description string
 }
 
 func NewPathParameter(rs specification.Ref[specification.PathParameter], components Componenter, cfg Config) (zero *PathParameter, _ Imports, _ error) {
 	s := rs.Value()
-	out := PathParameter{Description: s.Description}
-	out.Name = s.Name
-	out.FieldName = PublicFieldName(s.Name)
 
-	var ims Imports
-	var err error
-	out.Type, ims, err = NewSchema(s.Schema, components, cfg)
+	tp, ims, err := NewSchema(s.Schema, components, cfg)
 	if err != nil {
 		return nil, nil, fmt.Errorf("schema: %w", err)
 	}
-	kind := out.Type.Kind()
-	if kind != SchemaKindPrimitive {
-		return nil, nil, fmt.Errorf("path parameter could only be a primitive type, found %q", kind)
+	if tp.Kind() != SchemaKindPrimitive {
+		return nil, nil, fmt.Errorf("path parameter could only be a primitive type, found %q", tp.Kind())
 	}
-	return &out, ims, nil
+	return &PathParameter{
+		Name:        s.Name,
+		FieldName:   PublicFieldName(s.Name),
+		Type:        tp,
+		Description: s.Description,
+	}, ims, nil
+}
+
+func (p PathParameter) RenderToStringInline(from string) (string, error) {
+	return p.Type.RenderFormat(from + "." + p.FieldName)
 }
 
 type HeaderParameter struct {
